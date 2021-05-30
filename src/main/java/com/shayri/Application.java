cda468d92d07f4b4151e6a74682ce790ae51d006
akash15kumar <akash15.kumar@paytm.com>
2021-05-31 03:57:50 +0530

    thymeleaf for html viewrendering added

--- src/main/java/com/shayri/Application.java
+++ src/main/java/com/shayri/Application.java
@@ -3,16 +3,24 @@ package com.shayri;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @SpringBootApplication
 public class Application {
 
     @RequestMapping("/")
-    String home() {
+    public String home() {
         return "Shayriyan is up & running!";
+    }
+
+    @GetMapping("/greeting")
+    public ModelAndView greeting() {
+        ModelAndView mav = new ModelAndView("greeting");
+        return mav;
     }
 
     public static void main(String[] args) {
